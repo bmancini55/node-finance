66632349b968c099af6b5a5506b8446d168493ab
Brian Mancini <bmancini@gmail.com>
2019-11-06 10:58:05 -0500

    adding normal distribution

---
+++ __tests__/stats-normal.spec.js
@@ -0,0 +1,86 @@
+const { expect } = require('chai');
+const { Normal } = require('../lib/stats-normal');
+
+describe('Normal', () => {
+	describe('standard', () => {
+		it('should have mean of 0', () => {
+			expect(Normal.standard.mean).to.equal(0);
+		});
+
+		it('should have standard deviation of 1', () => {
+			expect(Normal.standard.stddev).to.equal(1);
+		});
+
+		it('should have variance of 1', () => {
+			expect(Normal.standard.variance).to.equal(1);
+		});
+
+		describe('cdf', () => {
+			let tests = [
+				[-3, 0.0013],
+				[-2, 0.0227],
+				[-1, 0.1586],
+				[0, 0.5],
+				[1, 0.8413],
+				[2, 0.9772],
+				[3, 0.9986],
+			];
+			for (let [input, expected] of tests) {
+				it(`should have correct cdf for x = ${input}`, () => {
+					expect(Normal.standard.cdf(input)).to.be.closeTo(expected, 0.0001);
+				});
+			}
+		});
+
+		describe('pdf', () => {
+			let tests = [
+				[-3, 0.0044],
+				[-2, 0.0539],
+				[-1, 0.2419],
+				[0, 0.3989],
+				[1, 0.2419],
+				[2, 0.0539],
+				[3, 0.0044],
+			];
+			for (let [input, expected] of tests) {
+				it(`should have correct pdf for x = ${input}`, () => {
+					expect(Normal.standard.pdf(input)).to.be.closeTo(expected, 0.0001);
+				});
+			}
+		});
+	});
+
+	describe('non-standard', () => {
+		let sut = new Normal(1, 0.5);
+
+		it('should have mean', () => {
+			expect(sut.mean).to.equal(1);
+		});
+
+		it('should have standard deviation', () => {
+			expect(sut.stddev).to.equal(0.5);
+		});
+
+		it('should have variance', () => {
+			expect(sut.variance).to.equal(0.25);
+		});
+
+		describe('cdf', () => {
+			let tests = [[0.5, 0.1586], [1, 0.5], [1.5, 0.8413], [2, 0.9772]];
+			for (let [input, expected] of tests) {
+				it(`should have correct cdf for x = ${input}`, () => {
+					expect(sut.cdf(input)).to.be.closeTo(expected, 0.0001);
+				});
+			}
+		});
+
+		describe('pdf', () => {
+			let tests = [[0.5, 0.4839], [1, 0.7978], [1.5, 0.4839], [2, 0.1079]];
+			for (let [input, expected] of tests) {
+				it(`should have correct cdf for x = ${input}`, () => {
+					expect(sut.pdf(input)).to.be.closeTo(expected, 0.0001);
+				});
+			}
+		});
+	});
+});
