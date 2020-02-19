a028de460a480108681733262c9a6fe96c1f1a95
Brian Mancini <bmancini@gmail.com>
2020-02-19 13:29:57 -0500

    refactor index to typescript

---
+++ lib/index.ts
@@ -0,0 +1,25 @@
+export * from "./calc-annual-return";
+export * from "./calc-cont-pv";
+export * from "./calc-hist-volatility";
+export * from "./calc-price-probability";
+export * from "./calc-return";
+export * from "./create-pricing-distribution";
+
+import * as blackscholesImport from "./black-scholes";
+export const blackscholes = {
+  ...blackscholesImport,
+};
+
+import * as mathNewtonRaphsonImport from "./math-newton-raphson";
+export const math = {
+  ...mathNewtonRaphsonImport,
+};
+
+import * as statsBasicImport from "./stats-basic";
+import * as normalImport from "./stats-normal";
+import * as lognormalImport from "./stats-lognormal";
+export const stats = {
+  ...statsBasicImport,
+  ...normalImport,
+  ...lognormalImport,
+};
